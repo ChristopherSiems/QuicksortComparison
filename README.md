e00254b723941e0bb02a875f2de6c250630b6a7f
ChristopherSiems <siemsck@gmail.com>
2024-03-03 21:33:18 -0500

    modified

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Comparing the runtimes of 2 different Quicksort implementations.
 
 2. Run `quicksort_segment.py`, this will save `segment.csv` which contains the runtimes of 100,000 lists of length 1 to 1000 sorted using the quicksort method as taught by Prof. Tseng of Clark University.
 
-3. Run `graph.py`, this will save `fig.png` and `O.png`, the former being a graph of the relationship between list size and runtime and the latter being a graph of $n \log n$ (the runtime of quicksort).
+3. Run `graph.py` this will produce and save `fig.png` which graphs the mean runtimes of each quicksort by the size of the input along side the theoretical runtime of $\cal{O}(n \log(n))$.
--- code/graph.py
+++ code/graph.py
@@ -2,26 +2,27 @@ import pandas
 from matplotlib import pyplot
 from numpy import log2
 
+fig, axis1 = pyplot.subplots()
+
 data = pandas.read_csv('swap.csv').groupby('input')['time'].mean()
-pyplot.plot(data.index, data.values, color = 'C1', label = 'swap')
+axis1.plot(data.index, data.values, color = 'C1', label = 'swap')
 
 data = pandas.read_csv('segment.csv').groupby('input')['time'].mean()
-pyplot.plot(data.index, data.values, color = 'C2', label = 'segment')
+axis1.plot(data.index, data.values, color = 'C2', label = 'segment')
+
+axis2 = axis1.twinx()
+x = range(1, 1001)
+
+axis2.plot(x, x * log2(x), color = 'black', label = 'nlog(n)')
 
 pyplot.title('Mean runtime by input size')
 pyplot.xlabel('Input size (# elements in list)')
-pyplot.ylabel('Mean runtime (sec)')
-pyplot.legend()
+axis1.set_ylabel('Mean runtime (sec)')
+axis2.set_ylabel('nlog(n)')
+
+lines, labels = axis1.get_legend_handles_labels()
+lines2, labels2 = axis2.get_legend_handles_labels()
+
+pyplot.legend(lines + lines2, labels + labels2)
 
 pyplot.savefig('fig.png')
-
-pyplot.clf()
-
-x = range(0, 1001)
-
-pyplot.plot(x, (x * log2(x)), color = 'black')
-pyplot.title('nlog(n)')
-pyplot.xlabel('Input size (# elements in list)')
-pyplot.ylabel('Mean runtime')
-
-pyplot.savefig('O.png')
